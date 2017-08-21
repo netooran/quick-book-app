5ff393d7277e99c41ed810d60e57fdcd425fc8dc
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-08-21 17:21:06 +0530

    [Sooraj] Updating page on timer end

--- src/components/booked-room/booked-room.ts
+++ src/components/booked-room/booked-room.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, Events } from 'ionic-angular';
 
-import { RoomPage} from '../../pages/room/room'
+import { RoomPage } from '../../pages/room/room'
 import { RoomProvider } from '../../providers/room/room';
 import { ToastProvider } from '../../providers/toast/toast';
 
@@ -11,8 +11,11 @@ import { ToastProvider } from '../../providers/toast/toast';
 })
 export class BookedRoomComponent extends RoomPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public roomProvider: RoomProvider, private toast: ToastProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public roomProvider: RoomProvider, private toast: ToastProvider, public events: Events) {
     super(navCtrl, navParams, roomProvider);
+    events.subscribe('timer:ended', () => {
+      this.updateRoomStatus();
+    });
   }
 
   endMeeting() {
